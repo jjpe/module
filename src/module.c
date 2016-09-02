0d76061677dd84dbece9d268a295e06c72c1c46e
Joey Ezechiëls <joey.ezechiels@gmail.com>
2016-09-02 18:52:14 +0200

    Add core infrastructure to module.c

--- src/module.c
+++ src/module.c
@@ -1,7 +1,20 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <czmq.h>
+#include "../include/emacs-module.h"
+
+// This states that the module is GPL-compliant.
+// Emacs won't load the module if this symbol is undefined.
+int plugin_is_GPL_compatible = 0;
 
 
+// Module entry point
+int emacs_module_init(struct emacs_runtime *ert) {
+
+    return 0;
+}
 
 /*  LocalWords:  czmq
  */
